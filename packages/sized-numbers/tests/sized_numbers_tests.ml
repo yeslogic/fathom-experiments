3ae144220ac1dcffba185c989fbb87e7903dd469
brendanzab <bjzaba@yahoo.com.au>
2024-11-28 15:17:46 +1100

    test(sized-numbers): Create limit tests based on module

--- packages/sized-numbers/tests/sized_numbers_tests.ml
+++ packages/sized-numbers/tests/sized_numbers_tests.ml
@@ -5,12 +5,21 @@ module Alcotest = Alcotest.V1
 let () =
   let open Alcotest in
 
-  let uint8 = testable UInt8.pp UInt8.equal in
-  let uint16 = testable UInt16.pp UInt16.equal in
-  let uint32 = testable UInt32.pp UInt32.equal in
-  let uint64 = testable UInt64.pp UInt64.equal in
-  let int8 = testable Int8.pp Int8.equal in
-  let int16 = testable Int16.pp Int16.equal in
+  let sized_int (type t) (module I : Unsigned.S with type t = t) : t testable =
+    testable I.pp I.equal
+  in
+
+  (* let uint8 = sized_int (module UInt8) in *)
+  (* let uint16 = sized_int (module UInt16) in *)
+  let uint32 = sized_int (module UInt32) in
+  let uint64 = sized_int (module UInt64) in
+  (* let int8 = sized_int (module Int8) in *)
+  (* let int16 = sized_int (module Int16) in *)
+
+  let limits_tests (module I : Unsigned.S) = [
+    test_case "wrap max_int successor" `Quick (fun () -> check (sized_int (module I)) "same uint8" I.min_int I.(succ max_int));
+    test_case "wrap min_int predecessor" `Quick (fun () -> check (sized_int (module I)) "same uint8" I.max_int I.(pred min_int));
+  ] in
 
   run "Sized_numbers" [
     "UInt32.to_string", [
@@ -59,36 +68,12 @@ let () =
       test_case "separator after sign" `Quick (fun () -> check_raises "failure" (Failure "UInt64.of_string") (fun () -> ignore UInt64.(of_string "+_0")));
       test_case "separator after base" `Quick (fun () -> check_raises "failure" (Failure "UInt64.of_string") (fun () -> ignore UInt64.(of_string "0x_0")));
     ];
-    "UInt8 limits", [
-      test_case "wrap max_int successor" `Quick (fun () -> check uint8 "same uint8" UInt8.min_int UInt8.(succ max_int));
-      test_case "wrap min_int predecessor" `Quick (fun () -> check uint8 "same uint8" UInt8.max_int UInt8.(pred min_int));
-    ];
-    "UInt16 limits", [
-      test_case "wrap max_int successor" `Quick (fun () -> check uint16 "same uint16" UInt16.min_int (UInt16.(succ max_int)));
-      test_case "wrap min_int predecessor" `Quick (fun () -> check uint16 "same uint16" UInt16.max_int (UInt16.(pred min_int)));
-    ];
-    "UInt32 limits", [
-      test_case "wrap max_int successor" `Quick (fun () -> check uint32 "same uint32" UInt32.min_int UInt32.(succ max_int));
-      test_case "wrap min_int predecessor" `Quick (fun () -> check uint32 "same uint32" UInt32.max_int UInt32.(pred min_int));
-    ];
-    "UInt64 limits", [
-      test_case "wrap max_int successor" `Quick (fun () -> check uint64 "same uint64" UInt64.min_int UInt64.(succ max_int));
-      test_case "wrap min_int predecessor" `Quick (fun () -> check uint64 "same uint64" UInt64.max_int UInt64.(pred min_int));
-    ];
-    "Int8 limits", [
-      test_case "wrap max_int successor" `Quick (fun () -> check int8 "same uint8" Int8.min_int Int8.(succ max_int));
-      test_case "wrap min_int predecessor" `Quick (fun () -> check int8 "same uint8" Int8.max_int Int8.(pred min_int));
-    ];
-    "Int16 limits", [
-      test_case "wrap max_int successor" `Quick (fun () -> check int16 "same uint16" Int16.min_int Int16.(succ max_int));
-      test_case "wrap min_int predecessor" `Quick (fun () -> check int16 "same uint16" Int16.max_int Int16.(pred min_int));
-    ];
-    "Int32 limits", [
-      test_case "wrap max_int successor" `Quick (fun () -> check int32 "same uint32" Int32.min_int Int32.(succ max_int));
-      test_case "wrap min_int predecessor" `Quick (fun () -> check int32 "same uint32" Int32.max_int Int32.(pred min_int));
-    ];
-    "Int64 limits", [
-      test_case "wrap max_int successor" `Quick (fun () -> check int64 "same uint64" Int64.min_int Int64.(succ max_int));
-      test_case "wrap min_int predecessor" `Quick (fun () -> check int64 "same uint64" Int64.max_int Int64.(pred min_int));
-    ];
+    "UInt8 limits", limits_tests (module UInt8);
+    "UInt16 limits", limits_tests (module UInt16);
+    "UInt32 limits", limits_tests (module UInt32);
+    "UInt64 limits", limits_tests (module UInt64);
+    "Int8 limits", limits_tests (module Int8);
+    "Int16 limits", limits_tests (module Int16);
+    "Int32 limits", limits_tests (module Int32);
+    "Int64 limits", limits_tests (module Int64);
   ]
