b50bd4f2cc7d9997c37b0797eb9b8937bde1d1fb
brendanzab <bjzaba@yahoo.com.au>
2025-04-15 11:21:02 +1000

    change(ll1-zippy): Reorganise modules

--- lang-ll1-zippy/syntax.ml
+++ lang-ll1-zippy/syntax.ml
@@ -12,8 +12,32 @@ module type S = sig
   val seq : 'a t -> 'b t -> ('a * 'b) t
   val map : ('a -> 'b) -> 'a t -> 'b t
 
-  val parse_naive : 'a t -> token Seq.t -> ('a * token Seq.t) option
-  val parse_derivative : 'a t -> token Seq.t -> ('a * token Seq.t) option
+  (** LL parser semantics, implemented with recursive-descent and backtracking *)
+  module Ll : sig
+
+    val parse : 'a t -> token Seq.t -> ('a * token Seq.t) option
+
+  end
+
+  (** LL(1) parser semantics, implemented with derivatives *)
+  module Ll1 : sig
+
+    val nullable : 'a t -> 'a option
+    val is_nullable : 'a t -> bool
+    val is_productive : 'a t -> bool
+    val first : 'a t -> token_set
+    val should_not_follow : 'a t -> token_set
+    val has_conflict : 'a t -> bool
+
+    val derivative : token -> 'a t -> 'a t option
+
+    val parse : 'a t -> token Seq.t -> ('a * token Seq.t) option
+
+  end
+
+  (* TODO: LL(k) parsing (like Daedalus) *)
+  (* TODO: Zippy LL(1) parsing *)
+  (* TODO: Compile NFA-style syntax descriptions to DFA-style syntax (like Daedalus) *)
 
 end
 
@@ -43,145 +67,149 @@ module Make (T : Token.S) : S
   let seq s1 s2 = Seq (s1, s2)
   let map f s = Map (f, s)
 
-  (* Syntax properties *)
+  module Ll = struct
 
-  let rec nullable : type a. a t -> a option =
-    function
-    | Elem _ -> None
-    | Fail -> None
-    | Pure x -> Some x
-    | Alt (s1, s2) -> Option.alt (nullable s1) (fun () -> nullable s2)
-    | Seq (s1, s2) -> Option.both (nullable s1) (nullable s2)
-    | Map (f, s) -> nullable s |> Option.map f
+    let rec parse : type a. a t -> token Seq.t -> (a * token Seq.t) option =
+      let open Option.Notation in
+      fun s ts ->
+        match s with
+        | Elem tk ->
+            let* (t, ts) = Seq.uncons ts in
+            if T.Set.mem t tk then Some (t, ts) else None
+        | Fail -> None
+        | Pure x -> Some (x, ts)
+        | Alt (s1, s2) ->
+            Option.alt (parse s1 ts) (fun () -> parse s2 ts)
+        | Seq (s1, s2) ->
+            let* (x1, ts) = parse s1 ts in
+            let+ (x2, ts) = parse s2 ts in
+            ((x1, x2), ts)
+        | Map (f, s) ->
+            let+ (x, ts) = parse s ts in
+            (f x, ts)
 
-  let rec is_nullable : type a. a t -> bool =
-    (* fun s -> Option.is_some (nullable s) *)
-    function
-    | Elem _ -> false
-    | Fail -> false
-    | Pure _ -> true
-    | Alt (s1, s2) -> is_nullable s1 || is_nullable s2
-    | Seq (s1, s2) -> is_nullable s1 && is_nullable s2
-    | Map (_, s) -> is_nullable s
+  end
 
-  let rec is_productive : type a. a t -> bool =
-    function
-    | Elem _ -> true
-    | Fail -> false
-    | Pure _ -> true
-    | Alt (s1, s2) -> is_productive s1 || is_productive s2
-    | Seq (s1, s2) -> is_productive s1 && is_productive s2
-    | Map (_, s) -> is_productive s
+  module Ll1 = struct
 
-  let rec first : type a. a t -> token_set =
-    function
-    | Elem t -> t
-    | Fail -> T.Set.empty
-    | Pure _ -> T.Set.empty
-    | Alt (s1, s2) ->
-        T.Set.union (first s1) (first s2)
-    | Seq (s1, s2) ->
-        T.Set.empty
-        |> T.Set.union (if is_nullable s1 then first s2 else T.Set.empty)
-        |> T.Set.union (if is_productive s2 then first s1 else T.Set.empty)
-    | Map (_, s) -> first s
-
-  let rec should_not_follow : type a. a t -> token_set =
-    function
-    | Elem _ -> T.Set.empty
-    | Fail -> T.Set.empty
-    | Pure _ -> T.Set.empty
-    | Alt (s1, s2) ->
-        T.Set.union (should_not_follow s1) (should_not_follow s2)
-        |> T.Set.union (if is_nullable s2 then first s1 else T.Set.empty)
-        |> T.Set.union (if is_nullable s1 then first s2 else T.Set.empty)
-    | Seq (s1, s2) ->
-        T.Set.empty
-        |> T.Set.union (if is_nullable s2 then should_not_follow s1 else T.Set.empty)
-        |> T.Set.union (if is_productive s1 then should_not_follow s2 else T.Set.empty)
-    | Map (_, s) ->
-        should_not_follow s
-
-  let rec has_conflict : type a. a t -> bool =
-    function
-    | Elem _ -> false
-    | Fail -> false
-    | Pure _ -> false
-    | Alt (s1, s2) ->
-        has_conflict s1
-          || has_conflict s2
-          || (is_nullable s1 && is_nullable s2)
-          || not (T.Set.disjoint (first s1) (first s2))
-    | Seq (s1, s2) ->
-        has_conflict s1
-          || has_conflict s2
-          || not (T.Set.disjoint (should_not_follow s1) (first s2))
-    | Map (_, s) ->
-        has_conflict s
-
-
-  (* Parsing *)
-
-  let rec parse_naive : type a. a t -> token Seq.t -> (a * token Seq.t) option =
-    let open Option.Notation in
-    fun s ts ->
-      match s with
-      | Elem tk ->
-          let* (t, ts) = Seq.uncons ts in
-          if T.Set.mem t tk then Some (t, ts) else None
-      | Fail -> None
-      | Pure x -> Some (x, ts)
-      | Alt (s1, s2) ->
-          Option.alt (parse_naive s1 ts) (fun () -> parse_naive s2 ts)
-      | Seq (s1, s2) ->
-          let* (x1, ts) = parse_naive s1 ts in
-          let+ (x2, ts) = parse_naive s2 ts in
-          ((x1, x2), ts)
-      | Map (f, s) ->
-          let+ (x, ts) = parse_naive s ts in
-          (f x, ts)
-
-  (** Returns the state of the syntax after seeing a token *)
-  let rec derivative : type a. token -> a t -> a t option =
-    let open Option.Notation in
-    fun t s ->
-      match s with
-      | Elem tk when T.Set.mem t tk ->
-          Some (Pure t)
+    let rec nullable : type a. a t -> a option =
+      function
       | Elem _ -> None
       | Fail -> None
-      | Pure _ -> None
-      | Alt (s1, s2) ->
-          if T.Set.mem t (first s1) then
-            derivative t s1
-          else
-            derivative t s2
-      | Seq (s1, s2) ->
-          begin match nullable s1 with
-          | Some x when T.Set.mem t (first s2)->
-              let* s2' = derivative t s2 in
-              Some (Seq (Pure x, s2'))
-          | Some _ | None ->
-              let* s1' = derivative t s1 in
-              Some (Seq (s1', s2))
-          end
-      | Map (f, s) ->
-          let+ s' = derivative t s in
-          Map (f, s')
+      | Pure x -> Some x
+      | Alt (s1, s2) -> Option.alt (nullable s1) (fun () -> nullable s2)
+      | Seq (s1, s2) -> Option.both (nullable s1) (nullable s2)
+      | Map (f, s) -> nullable s |> Option.map f
 
-  let rec parse_derivative : type a. a t -> token Seq.t -> (a * token Seq.t) option =
-    let open Option.Notation in
-    fun s ts ->
-      match Seq.uncons ts with
-      | None ->
-          let+ x = nullable s  in
-          (x, Seq.empty)
-      | Some (t, ts) ->
-          if T.Set.mem t (first s) then
-            let* s' = derivative t s in
-            parse_derivative s' ts
-          else
-            None
+    let rec is_nullable : type a. a t -> bool =
+      (* fun s -> Option.is_some (nullable s) *)
+      function
+      | Elem _ -> false
+      | Fail -> false
+      | Pure _ -> true
+      | Alt (s1, s2) -> is_nullable s1 || is_nullable s2
+      | Seq (s1, s2) -> is_nullable s1 && is_nullable s2
+      | Map (_, s) -> is_nullable s
+
+    let rec is_productive : type a. a t -> bool =
+      function
+      | Elem _ -> true
+      | Fail -> false
+      | Pure _ -> true
+      | Alt (s1, s2) -> is_productive s1 || is_productive s2
+      | Seq (s1, s2) -> is_productive s1 && is_productive s2
+      | Map (_, s) -> is_productive s
+
+    let rec first : type a. a t -> token_set =
+      function
+      | Elem t -> t
+      | Fail -> T.Set.empty
+      | Pure _ -> T.Set.empty
+      | Alt (s1, s2) ->
+          T.Set.union (first s1) (first s2)
+      | Seq (s1, s2) ->
+          T.Set.empty
+          |> T.Set.union (if is_nullable s1 then first s2 else T.Set.empty)
+          |> T.Set.union (if is_productive s2 then first s1 else T.Set.empty)
+      | Map (_, s) -> first s
+
+    let rec should_not_follow : type a. a t -> token_set =
+      function
+      | Elem _ -> T.Set.empty
+      | Fail -> T.Set.empty
+      | Pure _ -> T.Set.empty
+      | Alt (s1, s2) ->
+          T.Set.union (should_not_follow s1) (should_not_follow s2)
+          |> T.Set.union (if is_nullable s2 then first s1 else T.Set.empty)
+          |> T.Set.union (if is_nullable s1 then first s2 else T.Set.empty)
+      | Seq (s1, s2) ->
+          T.Set.empty
+          |> T.Set.union (if is_nullable s2 then should_not_follow s1 else T.Set.empty)
+          |> T.Set.union (if is_productive s1 then should_not_follow s2 else T.Set.empty)
+      | Map (_, s) ->
+          should_not_follow s
+
+    let rec has_conflict : type a. a t -> bool =
+      function
+      | Elem _ -> false
+      | Fail -> false
+      | Pure _ -> false
+      | Alt (s1, s2) ->
+          has_conflict s1
+            || has_conflict s2
+            || (is_nullable s1 && is_nullable s2)
+            || not (T.Set.disjoint (first s1) (first s2))
+      | Seq (s1, s2) ->
+          has_conflict s1
+            || has_conflict s2
+            || not (T.Set.disjoint (should_not_follow s1) (first s2))
+      | Map (_, s) ->
+          has_conflict s
+
+    (** Returns the state of the syntax after seeing a token *)
+    let rec derivative : type a. token -> a t -> a t option =
+      let open Option.Notation in
+      fun t s ->
+        match s with
+        | Elem tk when T.Set.mem t tk ->
+            Some (Pure t)
+        | Elem _ -> None
+        | Fail -> None
+        | Pure _ -> None
+        | Alt (s1, s2) ->
+            if T.Set.mem t (first s1) then
+              derivative t s1
+            else
+              derivative t s2
+        | Seq (s1, s2) ->
+            begin match nullable s1 with
+            | Some x when T.Set.mem t (first s2)->
+                let* s2' = derivative t s2 in
+                Some (Seq (Pure x, s2'))
+            | Some _ | None ->
+                let* s1' = derivative t s1 in
+                Some (Seq (s1', s2))
+            end
+        | Map (f, s) ->
+            let+ s' = derivative t s in
+            Map (f, s')
+
+    let rec parse : type a. a t -> token Seq.t -> (a * token Seq.t) option =
+      (* TODO: Check for conflicts first? *)
+      let open Option.Notation in
+      fun s ts ->
+        match Seq.uncons ts with
+        | None ->
+            let+ x = nullable s  in
+            (x, Seq.empty)
+        | Some (t, ts) ->
+            if T.Set.mem t (first s) then
+              let* s' = derivative t s in
+              (parse [@tailcall]) s' ts
+            else
+              None
+
+  end
 
 end
 
