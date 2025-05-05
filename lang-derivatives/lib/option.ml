4eaeb3c86b8001852dc50ee5977aa5994bdae2c9
brendanzab <bjzaba@yahoo.com.au>
2025-05-05 15:39:17 +1000

    style(derivatives): Add annotations to option functions

--- lang-derivatives/lib/option.ml
+++ lang-derivatives/lib/option.ml
@@ -2,12 +2,12 @@
 
 include Stdlib.Option
 
-let both o1 o2 =
+let both (type a b) (o1 : a t) (o2 : b t) : (a * b) t =
   match o1, o2 with
   | Some x1, Some x2 -> Some (x1, x2)
   | None, _ | _, None -> None
 
-let alt o1 o2 =
+let alt (type a) (o1 : a t) (o2 : unit -> a t) : a t =
   match o1 with
   | Some x -> Some x
   | None -> o2 ()
